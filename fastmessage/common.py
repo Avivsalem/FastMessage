ba2bcea2d075f6844b5ab3b895f2b42541a6e12a
avivs <avivsalem@gmail.com>
2023-08-11 14:39:11 +0300

    big refactor in modules added MethodValidator changed FastMessageOutput to CustomOutput

--- fastmessage/common.py
+++ fastmessage/common.py
@@ -1,4 +1,5 @@
 import logging
+from dataclasses import dataclass
 from typing import Callable, Any, TypeVar
 
 from fastmessage.exceptions import UnnamedCallableException
@@ -8,8 +9,38 @@ _logger = logging.getLogger('fastmessage')
 _CALLABLE_TYPE = TypeVar('_CALLABLE_TYPE', bound=Callable[..., Any])
 
 
-def _get_callable_name(callback: _CALLABLE_TYPE) -> str:
+def get_callable_name(named_callable: _CALLABLE_TYPE) -> str:
+    """
+    tries to return a callable name
+
+    :param named_callable: the callable to get the name for
+
+    :return: the name of the callable, or raises UnnamedCallableException
+    """
     try:
-        return getattr(callback, "__name__")
+        return getattr(named_callable, "__name__")
     except AttributeError as ex:
-        raise UnnamedCallableException(f"Callable {repr(callback)} doesn't have a name") from ex
+        raise UnnamedCallableException(f"Callable {repr(named_callable)} doesn't have a name") from ex
+
+
+class InputDeviceName(str):
+    """
+    a place holder class for input_device name
+    """
+    pass
+
+
+class MultipleReturnValues(list):
+    """
+    a value that indicates that multiple output values should be returned
+    """
+    pass
+
+
+@dataclass
+class CustomOutput:
+    """
+    a result that contains the output device name to send the value to
+    """
+    output_device: str
+    value: Any
